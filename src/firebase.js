ac6e9edbabb998dbb600f530f475dc5d73626cca
lee-vartha <lee-vartha@live.nmit.ac.nz>
2023-10-31 11:36:07 +1300

    login detail data

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_RDMEW2XErlAket9oWCaB8RgYn4lNV24",
+  authDomain: "web504-a2-lee-vartha.firebaseapp.com",
+  projectId: "web504-a2-lee-vartha",
+  storageBucket: "web504-a2-lee-vartha.appspot.com",
+  messagingSenderId: "111904444493",
+  appId: "1:111904444493:web:e95307c101cc6d405c0c04"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
